0fdabdad32b7d15588eb6e3bc9e2491047075e21
pl4g4ch3 <plagache@student.42.fr>
2020-01-20 19:01:19 +0100

    added tags file and script, bin can read file, added rules for input and output of bin

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -5,4 +5,16 @@
 # include <string.h> 
 # include <stdio.h> 
 # include "libft.h"
+
+# define SUCCESS 0
+# define FAILURE 1
+# define BUFF_SIZE 100
+# define STRNEW_FAILURE -1
+# define STRJOIN_FAILURE -2
+typedef struct	s_lem_in
+{
+	char		*file;
+	t_list		*head;
+}				t_lem_in;
+
 # endif
