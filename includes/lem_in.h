676987197b431a576509250eb146968d88f0797e
pl4g4ch3 <plagache@student.42.fr>
2020-01-20 14:15:30 +0100

    add map test

---
+++ includes/lem_in.h
@@ -0,0 +1,8 @@
+#ifndef LEM_IN_H
+# define LEM_IN_H
+# include <stdlib.h> 
+# include <unistd.h> 
+# include <string.h> 
+# include <stdio.h> 
+# include "libft.h"
+# endif
---
+++ srcs/lem_in.c
@@ -0,0 +1,5 @@
+#include "lem_in.h"
+int main()
+{
+	return (EXIT_SUCCESS);
+}
